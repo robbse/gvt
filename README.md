ccf57f533976de7d627a4d0d2d7b1be50c36c6c6
Robert Neujahr <r.neujahr@gmx.net>
2018-10-15 12:50:46 +0200

    renamed app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ea1App
+# Graphical Visualisation Technologies - Medieninformatik Online Master
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ea1-app'`, () => {
+  it(`should have as title 'gvt-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ea1-app');
+    expect(app.title).toEqual('gvt-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ea1-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to gvt-app!');
   });
 });
